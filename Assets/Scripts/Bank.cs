45d71e5c71767924db9f0e444b9050dd742a5dd0
Alin Apostol <alin.cristian977@gmail.com>
2021-08-16 21:13:49 +0300

    Display and Update Balance on UI

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -3,25 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] private int _startingBalance = 150;
     public int CurrentBalance { get; private set; }
 
+    [SerializeField] private TextMeshProUGUI _displayBalance;
+
     private void Awake()
     {
         CurrentBalance = _startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         CurrentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         CurrentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if (CurrentBalance < 0)
         {
@@ -29,6 +35,12 @@ public class Bank : MonoBehaviour
             ReloadScene();
         }
     }
+
+    private void UpdateDisplay()
+    {
+        _displayBalance.text = $"Gold: {CurrentBalance}";
+    }
+    
 
     private void ReloadScene()
     {

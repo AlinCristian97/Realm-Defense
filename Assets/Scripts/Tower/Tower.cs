ffe4ceb12d317eac43b3da5f1025150dc48cbfd8
Alin Apostol <alin.cristian977@gmail.com>
2021-08-17 19:45:05 +0300

    Added Build Timer

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Tower
@@ -5,7 +7,38 @@ namespace Tower
     public class Tower : MonoBehaviour
     {
         [SerializeField] private int _cost = 75;
-        
+        [SerializeField] private float _buildDelay = 1f;
+
+        private void Start()
+        {
+            StartCoroutine(Build());
+        }
+
+        private IEnumerator Build()
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+
+                foreach (Transform grandchild in child)
+                {
+                    grandchild.gameObject.SetActive(false);
+                }
+            }
+            
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+
+                yield return new WaitForSeconds(_buildDelay);
+
+                foreach (Transform grandchild in child)
+                {
+                    grandchild.gameObject.SetActive(true);
+                }
+            }
+        }
+
         public bool CreateTower(Tower tower, Vector3 position)
         {
             var bank = FindObjectOfType<Bank>();
